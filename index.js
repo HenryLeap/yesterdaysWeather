d33937a4d9169e989fa8a1475f5d8a36ce6a32bc
codeBodger <diesel.ack+github@gmail.com>
2025-03-01 14:43:55 -0500

    Using cookies, doing things on pageload, etc.

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-var metric = false;
+function getCookies() {
+    const cookie = JSON.parse(document.cookie)
+    cookie.metric ??= false;
+    cookie.days ??= 1;
+    cookie.temp ??= true;
+    cookie.feel ??= false;
+    cookie.wind ??= false;
+    cookie.precip ??= true;
+    cookie.humid ??= false;
+    cookie.dewpt ??= false;
+    cookie.sun ??= true;
+    cookie.moon ??= false;
+    return cookie;
+}
+
+function updateState(cookie) {
+    document.cookie = JSON.stringify(cookie);
+    document.getElementById("units").innerText = cookie.metric ? "US Standard" : "Metric";
+}
+
+function pageLoad() {
+    updateState(getCookies());
+}
 
 function changeUnits() {
-    metric = !metric;
-    document.getElementById("units").innerText = metric ? "US Standard" : "Metric";
+    const cookie = getCookies();
+    cookie.metric = !cookie.metric;
+    updateState(cookie);
 }
