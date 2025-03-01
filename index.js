2fbb6207baa574cce94c39bd3487efdb2dacfed5
codeBodger <diesel.ack+github@gmail.com>
2025-03-01 14:55:17 -0500

    Setting days and disabling their buttons

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ function getCookies() {
 function updateState(cookie) {
     document.cookie = JSON.stringify(cookie);
     document.getElementById("units").innerText = cookie.metric ? "US Standard" : "Metric";
+    cookie.days === 1 ? document.getElementById("1-day").setAttribute("disabled", "") : document.getElementById("1-day").removeAttribute("disabled");
+    cookie.days === 3 ? document.getElementById("3-day").setAttribute("disabled", "") : document.getElementById("3-day").removeAttribute("disabled");
+    cookie.days === 7 ? document.getElementById("7-day").setAttribute("disabled", "") : document.getElementById("7-day").removeAttribute("disabled");
 }
 
 function pageLoad() {
@@ -25,5 +28,11 @@ function pageLoad() {
 function changeUnits() {
     const cookie = getCookies();
     cookie.metric = !cookie.metric;
+    updateState(cookie);
+}
+
+function changeDays(count) {
+    const cookie = getCookies();
+    cookie.days = count;
     updateState(cookie);
 }
